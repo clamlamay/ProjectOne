eab27d752f847450ed157bc27cb328de65b2d033
Christopher Lamlamay <Christopher@Christophers-MacBook-Pro.local>
2016-10-09 12:42:13 -0700

    saving changes from Friday afternoon

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+console.log('loaded window')
+
+//Music
+// document.getElementById('FF7-theme').play();
+
+$('#beginButton').click(function(){
+	console.log('Ready to play');
+});
+
+
+
+
+// var cardOne = 'King';
+// var cardTwo = 'Queen';
+// var cardThree = 'King';
+// var cardFour = 'Queen';
+
+// if (cardOne === cardThree) {
+// 	alert('YASS, King!');	
+// } else {
+//   alert('Sorry, try again.');
+// }
+
+// if (cardTwo == cardFour) {
+// 	alert('YASS, Queen!');	
+// } else {
+//   alert('Sorry, try again.');
+// }
+
+// var board = document.getElementById('game-board');
+
+// function createBoard() {
+//   for (var i=0; i<cards.length; i++) {
+//     var cardElement = document.createElement('div'); 
+//     cardElement.className = 'card';
+//     board.appendChild(cardElement);
+//   }
