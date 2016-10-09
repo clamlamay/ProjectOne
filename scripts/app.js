a36acfc6b687f0095aa2010dab0eadcd53cf2df2
Christopher Lamlamay <Christopher@Christophers-MacBook-Pro.local>
2016-10-09 13:42:11 -0700

    practicing flip animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,15 @@
 console.log('loaded window')
 
-//Music
-// document.getElementById('FF7-theme').play();
+document.getElementById('FF7-theme').play();
 
 $('#beginButton').click(function(){
 	console.log('Ready to play');
 });
 
 
+$('.card').click(function(){
+	console.log('Card was clicked');
+});
 
-
-// var cardOne = 'King';
-// var cardTwo = 'Queen';
-// var cardThree = 'King';
-// var cardFour = 'Queen';
-
-// if (cardOne === cardThree) {
-// 	alert('YASS, King!');	
-// } else {
-//   alert('Sorry, try again.');
-// }
-
-// if (cardTwo == cardFour) {
-// 	alert('YASS, Queen!');	
-// } else {
-//   alert('Sorry, try again.');
-// }
-
-// var board = document.getElementById('game-board');
-
-// function createBoard() {
-//   for (var i=0; i<cards.length; i++) {
-//     var cardElement = document.createElement('div'); 
-//     cardElement.className = 'card';
-//     board.appendChild(cardElement);
-//   }
+$('.card').hover(function(){
+$(this).addClass('flip');
