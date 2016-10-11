9704349c4577a56c86b7b5f46a1039fbc30286c0
Christopher Lamlamay <Christopher@Christophers-MacBook-Pro.local>
2016-10-11 10:33:23 -0500

    working on timer function

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,28 @@
 console.log('loaded window')
 
-document.getElementById('FF7-theme').play();
+// document.getElementById('FF7-theme').play();
 
 $('#beginButton').click(function(){
 	console.log('Ready to play');
 });
 
-
 $('.card').click(function(){
 	console.log('Card was clicked');
 });
 
-$('.card').hover(function(){
-$(this).addClass('flip');
+
+var count = 6;
+var counter = setInterval(timer,1000);
+
+function timer(){
+
+	count = count-1;
+	if (count <= 0)
+{
+	clearInterval(counter);
+}
+$("#timer").text("Timer: " + count);
+};
+
+timer();
+
